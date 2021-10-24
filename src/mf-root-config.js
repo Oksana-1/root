5f34d41de22f6e008c777c2d468763438a522072
oksana-1 <4438488@gmail.com>
2021-10-24 18:06:49 +0300

    Pass props

--- src/mf-root-config.js
+++ src/mf-root-config.js
@@ -2,13 +2,19 @@ import { registerApplication, start } from "single-spa";
 
 registerApplication({
   name: "@mf/navigation",
-  app: () => System.import("//localhost:9001/js/app.js"),
+  app: () => System.import("@mf/navigation"),
   activeWhen: ["/"],
+  customProps: () => {
+    return { myPet: "dog" };
+  },
 });
 registerApplication({
   name: "@mf/dogs",
-  app: () => System.import("//localhost:9002/js/app.js"),
+  app: () => System.import("@mf/dogs"),
   activeWhen: ["/dogs"],
+  customProps: () => {
+    return { myPet: "dog" };
+  },
 });
 
 start({
